cd5e53a6e9be4dbcf86e865ef6f109050cb31bd4
YanC Lin <yonalin1@gmail.com>
2018-01-11 22:07:15 +0000

    fifth commit

--- Libraries/lib/scraper.rb
+++ Libraries/lib/scraper.rb
@@ -1,5 +1,5 @@
 class Libraries::Scraper
-     attr_accessor :name
+     attr_accessor :name, :location
 
   def self.scrape_branch
       doc = Nokogiri::HTML(open("http://www.queenslibrary.org/ql_findabranch"))
