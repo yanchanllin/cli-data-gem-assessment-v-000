d3a2516e40dffb6970f282d00ee883e0f5ade6d3
YanC Lin <yonalin1@gmail.com>
2018-01-12 20:25:56 +0000

    cli almost finish

--- Libraries/lib/Libraries/cli.rb
+++ Libraries/lib/Libraries/cli.rb
@@ -1,53 +1,46 @@
-#our CLI Controller
 class Libraries::CLI
 
   def call
-   Libraries::Scraper.scrape_branch
-   puts "Welcome to the Libraries in Queens"
-   start
+    Libraries::Scraper.scrape_branch
+    puts "Welcome to the Libraries in Queens"
+    start
   end
-
-  def list
-      puts ""
-      puts "********Libraries in Queens Area********."
-      puts ""
-      Libraries::Location.all.each.with_index(1) do |location, i|
-        puts "#{i}. #{location.name}"
-      end
-      puts ""
-    end
 
   def print_location(location)
+    puts ""
+    puts "-------------- #{location.name} --------------"
+
+    puts ""
+    puts location.summary
+    puts ""
+
+  end
+
+  def start
+    puts ""
+    puts "What location would you like to find?"
+    input = gets.strip.to_i
+
+    print_location(input)
+
+    puts ""
+    puts "What location would you like more information on?"
+    input = gets.strip
+
+    location = Libraries::Location.find(input.to_i)
+
+    print_location(location)
+
+    puts ""
+    puts "Would you like to see another location? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    else
       puts ""
-      puts "-------------- #{location.name} --------------"
-
-      puts ""
-      puts location.summary
-      puts ""
-
-    end
-
-   def start
-     puts ""
-     puts "What location would you like more information on by name?"
-      input = gets.strip.to_i
-
-      print_location(input)
-
-      #///////////////start here,not finish yet
-        puts ""
-        puts "Enter list to see the location again."
-        puts "Enter exit to end the program."
-        puts ""
-        input = gets.strip
-        if input == "list"
-          list
-        elsif input.to_i == 0
-          if location = Libraries::Location.find_by_name(input)
-            print_location(location)
-          end
-        end
-      end
-      puts "Goodbye!!!"
+      puts "Thankyou! Goodbye!!"
+      exit
     end
   end
+end
