bf0fd4e7c8c56349a9b4d806eeebd126ea0efc67
YanC Lin <yonalin1@gmail.com>
2018-02-01 01:02:15 +0000

    automatically backed up by learn

---
+++ Libraries/lib/Libraries/brian.rb
@@ -0,0 +1,39 @@
+MEATS = [
+  'Chicken',
+  'Beef',
+  'Pork'
+]
+
+COLORS = [
+  "Blue",
+  "Red",
+  "Yellow",
+  "Orange",
+  "Pruple",
+  "Black",
+  "White",
+  "Indigo"
+]
+
+PETS = [
+  'Cats',
+  'Dogs',
+  'Fish',
+  'Bear'
+]
+
+
+
+def greeting
+  puts "Hi, welcome to my example"
+end
+
+def start
+  greeting
+  input = ''
+  until input=='exit'
+    menu
+    input==gets.strip
+    if input == 
+  end
+end
