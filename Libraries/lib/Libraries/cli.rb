48534ce78ade57bab0f8f142fb3521f9d735e0e1
YanC Lin <yonalin1@gmail.com>
2018-02-01 01:18:35 +0000

    automatically backed up by learn

--- Libraries/lib/Libraries/cli.rb
+++ Libraries/lib/Libraries/cli.rb
@@ -1,28 +1,32 @@
 class Libraries::CLI
-
+  
   def call
     Libraries::Scraper.scrape_branch
     puts ""
     puts "--------------Welcome to the Libraries in Queens--------------"
     start
   end
-
+  
   def print_location(location)
-    puts ""
-    puts "-------------- #{location.branch_name} --------------"
-
-    puts ""
-    puts "Location:   #{location.address}"
-    puts "Contact:    #{location.phone}"
-    puts ""
+    if location == nil 
+      puts "sorry, there is only 68 libraries in Queens"
+    else
+      puts ""
+      puts "-------------- #{location.branch_name} --------------"
+      
+      puts ""
+      puts "Location:   #{location.address}"
+      puts "Contact:    #{location.phone}"
+      puts ""
+    end 
   end
-
+  
   def print_locations
     Libraries::Location.all.each_with_index do |location,index|
       puts "#{index+1}. #{location.branch_name}"
     end
   end
-
+  
   def start
     puts ""
     puts "Which location would you like to find?"
@@ -31,23 +35,23 @@ class Libraries::CLI
     print_locations
     input = gets.strip.to_i
     # if input > 68
-    #   puts "sorry, there is only 68 libraries in Queens"
-
+    
+    
     location = Libraries::Location.find(input)
     print_location(location)
-
+    
     puts ""
     puts "Which location would you like more information on? 1-68"
     puts "(Note:There are 68 libraries list in Queens, please enter from 1-68)"
     puts ""
     input = gets.strip.to_i
-
+    
     location = Libraries::Location.find(input)
     print_location(location)
-
+    
     puts ""
     puts "Would you like to see another location? Enter Y or N"
-
+    
     input = gets.strip.downcase
     if input == "y"
       start
@@ -57,5 +61,5 @@ class Libraries::CLI
       exit
     end
   end
-
+  
 end
