70c6d38022a025f636c5e3266bf363703063c645
YanC Lin <yonalin1@gmail.com>
2018-02-01 00:42:04 +0000

    automatically backed up by learn

--- Libraries/lib/Libraries/cli.rb
+++ Libraries/lib/Libraries/cli.rb
@@ -26,6 +26,7 @@ class Libraries::CLI
     puts ""
     puts "Which location would you like to find?"
     puts "(There are 68 libraries list in Queens, please enter from 1-68)"
+    puts ""
     print_locations
     input = gets.strip.to_i
     if input > 68
