6c8fef7ba079a1078bcec9ebc0390da5b6333f30
YanC Lin <yonalin1@gmail.com>
2018-02-01 01:11:36 +0000

    automatically backed up by learn

--- Libraries/lib/Libraries/cli.rb
+++ Libraries/lib/Libraries/cli.rb
@@ -1,28 +1,28 @@
 class Libraries::CLI
-  
+
   def call
     Libraries::Scraper.scrape_branch
     puts ""
     puts "--------------Welcome to the Libraries in Queens--------------"
     start
   end
-  
+
   def print_location(location)
     puts ""
     puts "-------------- #{location.branch_name} --------------"
-    
+
     puts ""
     puts "Location:   #{location.address}"
     puts "Contact:    #{location.phone}"
     puts ""
   end
-  
+
   def print_locations
     Libraries::Location.all.each_with_index do |location,index|
       puts "#{index+1}. #{location.branch_name}"
     end
   end
-  
+
   def start
     puts ""
     puts "Which location would you like to find?"
@@ -32,22 +32,22 @@ class Libraries::CLI
     input = gets.strip.to_i
     # if input > 68
     #   puts "sorry, there is only 68 libraries in Queens"
-    
+
     location = Libraries::Location.find(input)
     print_location(location)
-    
+
     puts ""
     puts "Which location would you like more information on? 1-68"
     puts "(Note:There are 68 libraries list in Queens, please enter from 1-68)"
     puts ""
     input = gets.strip.to_i
-    
+
     location = Libraries::Location.find(input)
     print_location(location)
-    
+
     puts ""
     puts "Would you like to see another location? Enter Y or N"
-    
+
     input = gets.strip.downcase
     if input == "y"
       start
@@ -57,5 +57,5 @@ class Libraries::CLI
       exit
     end
   end
-  
+
 end
